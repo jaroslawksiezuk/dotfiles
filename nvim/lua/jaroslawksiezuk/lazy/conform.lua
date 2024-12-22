eab2775373b206e94fddd5ca60f12fb0754a1721
jaroslawksiezuk <23661244+jaroslawksiezuk@users.noreply.github.com>
2024-12-22 23:22:49 +0100

    Update nvim config

---
+++ nvim/lua/jaroslawksiezuk/lazy/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {},
+  config = function()
+    require("conform").setup({
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        java = { "google-java-format" },
+      }
+    })
+  end
+}
