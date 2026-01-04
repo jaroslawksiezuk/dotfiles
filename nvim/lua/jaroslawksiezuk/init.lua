190ee181cbac6af781a1992a20fe482e3ace02b7
Jarosław Księżuk <23661244+jaroslawksiezuk@users.noreply.github.com>
2026-01-04 21:54:09 +0100

    Update

--- nvim/lua/jaroslawksiezuk/init.lua
+++ nvim/lua/jaroslawksiezuk/init.lua
@@ -1,3 +1,17 @@
 require("jaroslawksiezuk.set")
 require("jaroslawksiezuk.remap")
 require("jaroslawksiezuk.lazy_init")
+
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●",
+		spacing = 2,
+		source = "if_many",
+	},
+	signs = false,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
+
+vim.opt.clipboard = "unnamedplus"
