ed86f7031a33fa01035b214beb2a465c4a1890d3
Jarosław Księżuk <23661244+jaroslawksiezuk@users.noreply.github.com>
2026-01-13 22:24:23 +0100

    Integrate Lombok with jdtls setup in lsp.lua
    Added support for jdtls with Lombok integration.

--- nvim/lua/jaroslawksiezuk/lazy/lsp.lua
+++ nvim/lua/jaroslawksiezuk/lazy/lsp.lua
@@ -12,6 +12,18 @@ return {
 			ensure_installed = { "gopls", "jdtls", "ts_ls" },
 			handlers = {
 				function(server_name)
+					if server_name == "jdtls" then
+						local lombok = vim.fn.expand("~/.local/share/java/lombok.jar")
+
+						require("lspconfig").jdtls.setup({
+							cmd = {
+								vim.fn.stdpath("data") .. "/mason/bin/jdtls",
+								("--jvm-arg=-javaagent:%s"):format(lombok),
+							},
+						})
+						return
+				  	end
+
 					require("lspconfig")[server_name].setup({})
 				end,
 			},
